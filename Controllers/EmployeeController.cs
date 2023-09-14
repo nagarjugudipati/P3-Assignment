728904554c205dfcd84553db79f158fda9c84232
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-09-14 20:08:35 +0530

    P3-Assignment4

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Assignment4.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        static List<Employee> Employees = new List<Employee>
+        {
+            new Employee(){EmpId=1, EmpName="sam",Destination="Manager"},
+            new Employee(){EmpId=2, EmpName="Amit",Destination="HR"},
+            new Employee(){EmpId=3, EmpName="Virat",Destination="Developer"},
+            new Employee(){EmpId=4, EmpName="Vijay",Destination="Tester"},
+            new Employee(){EmpId=5, EmpName="Suriya",Destination="Manager"},
+
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return Ok(Employees);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest("Invalid Employee data.");
+            }
+
+            employee.EmpId = Employees.Max(e => e.EmpId) + 1; // Generate a new ID
+            Employees.Add(employee);
+
+            return CreatedAtAction(nameof(Get), new { id = employee.EmpId }, employee);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment4.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int EmpId { get; set; }
+        [Required]
+
+        public string EmpName { get; set; }
+        [Required]
+        public string Destination { get; set; }
+    }
+}
